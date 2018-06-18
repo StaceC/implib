559266e4ba6e4dc99c68ecf55d07260a98e1b6c9
Stace C <stace@hiddenfield.com>
2018-06-18 11:14:44 +0100

    Wired the Tracks DB object to express controller, replacing the old Todo

--- app/server/controllers/tracks.ts
+++ app/server/controllers/tracks.ts
@@ -2,32 +2,31 @@ import {Router, Request, Response } from 'express';
 import db from "../../db/models";
 import fs = require("fs");
 import path = require("path");
-//import { Todo } from "../../components/todos/model";
 import config from "../../config";
 const SAVE_DIR = config.settings.saveDir;
 
 const router: Router = Router();
 
-// GET todos listing.
+// GET tracks listing.
 router.get('/', (req: Request, res: Response) => {
-  db.Todo.findAll({ raw: true })
-  .then(todos => res.send(todos));
+  db.Track.findAll({ raw: true })
+  .then(tracks => res.send(tracks));
+});
+
+// Get track info.
+router.get('/:trackId', (req: Request, res: Response) => {
+  const trackId = req.params.trackId;
+  db.Track.findById(trackId)
+  .then(track => res.send(track));
 });
 
 // Get todo info.
-router.get('/:todoId', (req: Request, res: Response) => {
-  const todoId = req.params.todoId;
-  db.Todo.findById(todoId)
-  .then(todo => res.send(todo));
-});
-
-// Get todo info.
-router.get('/:todoId/config', (req: Request, res: Response) => {
-  const todoId = req.params.todoId;
-  db.Todo.findById(todoId)
-  .then((todo) => {
-    if(todo) {
-      const configFilePath = path.join(SAVE_DIR, todo.text, "config.json");
+router.get('/:trackId/config', (req: Request, res: Response) => {
+  const trackId = req.params.trackId;
+  db.Track.findById(trackId)
+  .then((track) => {
+    if(track) {
+      const configFilePath = path.join(SAVE_DIR, track.name, "config.json");
       fs.readFile(configFilePath, 'utf8', function(err, data) {
         if (err) {
           console.log("Unable to load config file [" + configFilePath + "]");
@@ -36,7 +35,7 @@ router.get('/:todoId/config', (req: Request, res: Response) => {
           try {
             const configFile = JSON.parse(data);
             res.send(configFile);
-            console.log("Sent config file for [" + todoId + "]");
+            console.log("Sent config file for [" + trackId + "]");
           } catch(e) {
             console.log("Unable to parse config file [" + configFilePath + "]");
             res.status(500).send("Unable to parse config file.");
@@ -46,9 +45,9 @@ router.get('/:todoId/config', (req: Request, res: Response) => {
       });
     } else {
       // error
-      console.log("No todo was found with id [" + todoId + "] in the DB");
-      res.status(404).send("No element with id[" + todoId + "]");
-    }    
+      console.log("No track was found with id [" + trackId + "] in the DB");
+      res.status(404).send("No element with id[" + trackId + "]");
+    }
   });
 });
 
