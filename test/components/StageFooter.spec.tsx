b2fef0fd8148a226ef1ab92f46bf55a1db6a3fb6
Stace C <stace@hiddenfield.com>
2018-06-27 14:42:02 +0100

    Basic Jest/Enzyme test case for Stage Footer buttons added

---
+++ test/components/StageFooter.spec.tsx
@@ -0,0 +1,39 @@
+import '../utils/enzymeConfig';
+import { spy } from 'sinon';
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import { Footer } from '../../app/components/Stage';
+import {} from 'jest';
+
+const StageFooterAny = Footer as any;
+
+function setup() {
+
+  const actions = {
+    onImportTracks: spy(),
+    onClearTracks: spy(),
+  };
+
+  const component = shallow(<StageFooterAny {...actions} />);
+  return {
+    component,
+    actions,
+    buttons: component.find('button')
+  };
+}
+
+describe('Stage component', () => {
+
+  it('should first button should call clear completed imports', () => {
+    const { buttons, actions } = setup();
+    buttons.at(0).simulate('click');
+    expect(actions.onClearTracks.called).toBe(true);
+  });
+
+  it('should second button should call import', () => {
+    const { buttons, actions } = setup();
+    buttons.at(1).simulate('click');
+    expect(actions.onImportTracks.called).toBe(true);
+  });
+
+});
