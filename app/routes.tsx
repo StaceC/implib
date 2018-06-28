48080b495552710aa69ef6cf97daf3d59a655981
Stace C <stace@hiddenfield.com>
2018-06-28 14:23:18 +0100

    Removed Home container/component and set base route to be the Prototype

--- app/routes.tsx
+++ app/routes.tsx
@@ -1,14 +1,12 @@
 import * as React from 'react';
 import { Switch, Route } from 'react-router';
 import App from './containers/App';
-import HomePage from './containers/HomePage';
 import PrototypePage from './containers/PrototypePage';
 
 export default () => (
   <App>
     <Switch>
-      <Route path="/prototype" component={PrototypePage} />      
-      <Route path="/" component={HomePage} />
+      <Route path="/" component={PrototypePage} />
     </Switch>
   </App>
 );
